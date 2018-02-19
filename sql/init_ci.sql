2ed2dfc6334aa93453c5d39918c3ef044a9f205d
egor <yegor.litvinov@yandex.ru>
2018-02-19 21:29:55 +0300

    fix travis

---
+++ sql/init_ci.sql
@@ -0,0 +1,2 @@
+create user metric with encrypted password 'password';
+alter user metric with createdb;
--- sql/init_prod.sql
+++ sql/init_prod.sql
@@ -1,3 +1,2 @@
 create user metric with encrypted password 'password';
 create database metric owner metric;
--- alter user metric with createdb;
