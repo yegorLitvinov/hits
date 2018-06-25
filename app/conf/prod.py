301ffec036edbffb26c0fc0f03f74ce9c2ac2363
Egor Litvinov <yegor.litvinov@yandex.ru>
2018-06-26 01:47:20 +0300

    Fix services hostname

--- app/conf/prod.py
+++ app/conf/prod.py
@@ -1,5 +1,5 @@
 from .base import *  # noqa
 
-DSN_KWARGS['host'] = 'metric_pg'  # noqa
-REDIS_ADDR = 'redis://metric_redis'
+DSN_KWARGS['host'] = 'pg'  # noqa
+REDIS_ADDR = 'redis://redis'
 REDIS_DB = 1
